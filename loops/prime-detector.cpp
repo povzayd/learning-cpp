43df15bfc4110ac9a45fcfa1daf1fdf92ef9e3ac
Zayd <166023703+povzayd@users.noreply.github.com>
2025-11-18 12:48:41 +0530

    Update prime-detector.cpp

--- loops/prime-detector.cpp
+++ loops/prime-detector.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
 using namespace std;
-int main(){
-	
+int main()
+{
+	int num,num2;
+	cout << "Enter a number: ";
+	cin >> num;
+	for(int i=num/2; i>=0; i--){
+		if(num%i==0){
+			num2 = i;
+			break;
+		}
+	}	
+	if (num2==1){
+		cout << "The given number is a prime number!";
+	}
+	else {
+		cout << "The Given Number Is A Composite Number.";
+	}
+    
+	return 0;
 }
