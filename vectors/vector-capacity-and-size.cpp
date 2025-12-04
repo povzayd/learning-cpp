854c0449e74e2cd53160acd06b5a317331e4d50e
Zayd <166023703+povzayd@users.noreply.github.com>
2025-12-04 06:28:57 +0530

    Create vector-capacity-and-size.cpp

---
+++ vectors/vector-capacity-and-size.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+	vector <char> v;
+	v.push_back(0);
+	v.push_back(1);
+	v.push_back(2);
+	v.push_back(2);
+	v.push_back(2);
+	cout << v.size() << endl;
+	cout << v.capacity() << endl;
+	return 0;
+}
